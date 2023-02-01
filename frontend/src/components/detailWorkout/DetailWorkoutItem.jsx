0364b4c8d184bf331a8ce8aa861eb6d921df002c
AlexandrePre <pre.alexandre1@gmail.com>
2023-02-01 17:19:34 +0100

    css detailworkout

--- frontend/src/components/detailWorkout/DetailWorkoutItem.jsx
+++ frontend/src/components/detailWorkout/DetailWorkoutItem.jsx
@@ -10,12 +10,12 @@ function detailWorkoutItem({ info }) {
 
   return (
     <div className="detailWorkoutItem">
-      <p> {info[0].name_workout}</p>
-      <h4>exercice 1 {info[0].drillOne}</h4>
-      <h4>exercice 2 {info[0].drillTwo}</h4>
-      <h4>exercice 3 {info[0].drillThree}</h4>
-      <h4>exercice 4 {info[0].drillFour}</h4>
-      <h4>exercice 5 {info[0].drillFive}</h4>
+      <h4> {info[0].name_workout}</h4>
+      <p>exercice 1 {info[0].drillOne}</p>
+      <p>exercice 2 {info[0].drillTwo}</p>
+      <p>exercice 3 {info[0].drillThree}</p>
+      <p>exercice 4 {info[0].drillFour}</p>
+      <p>exercice 5 {info[0].drillFive}</p>
     </div>
   );
 }
