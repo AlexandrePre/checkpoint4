febbb5c6e28a7d71bfb87283572553cae2eb3407
AlexandrePre <pre.alexandre1@gmail.com>
2023-02-02 16:48:41 +0100

    fix framer-motion

--- frontend/src/components/dashboard/CardsItem.jsx
+++ frontend/src/components/dashboard/CardsItem.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import deleteworkout from "../../assets/img/delete.png";
 import api from "../../services/api";
 
@@ -11,22 +12,35 @@ function CardsItem({ nbdec, name_workout }) {
     api.delete(`workout/${nbdec}`, { withCredentials: true });
   };
   return (
-    <div className="CardItem">
-      <Link id="workoutTitle" to={`/dashboard/detailworkout/${nbdec}`}>
-        <div>
-          <h4 id="titleWorkout">{name_workout}</h4>
+    <motion.div
+      transition={{
+        duration: 0,
+        delay: 0.5,
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+      }}
+      initial={{ x: 500, scale: 0, opacity: 0 }}
+      animate={{ x: 0, scale: 1, opacity: 1 }}
+      exit={{ scale: 0, opacity: 1 }}
+    >
+      <div className="CardItem">
+        <Link id="workoutTitle" to={`/dashboard/detailworkout/${nbdec}`}>
+          <div>
+            <h4 id="titleWorkout">{name_workout}</h4>
+          </div>
+        </Link>
+        <div className="btn-delete">
+          <img
+            src={deleteworkout}
+            alt="delete"
+            role="presentation"
+            onClick={deleteWorkout}
+            id="delete"
+          />
         </div>
-      </Link>
-      <div className="btn-delete">
-        <img
-          src={deleteworkout}
-          alt="delete"
-          role="presentation"
-          onClick={deleteWorkout}
-          id="delete"
-        />
       </div>
-    </div>
+    </motion.div>
   );
 }
 
--- frontend/src/components/drills/CardDrill.jsx
+++ frontend/src/components/drills/CardDrill.jsx
@@ -1,19 +1,33 @@
 /* eslint-disable camelcase */
 
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 
 import "./AllDrills.css";
 
 function CardDrill({ name_drill, image }) {
   return (
-    <div className="CardDrill">
-      <div>
-        <h4 id="titleDrill">{name_drill}</h4>
+    <motion.div
+      transition={{
+        duration: 0,
+        delay: 0.5,
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+      }}
+      initial={{ x: 500, scale: 0, opacity: 0 }}
+      animate={{ x: 0, scale: 1, opacity: 1 }}
+      exit={{ scale: 0, opacity: 1 }}
+    >
+      <div className="CardDrill">
+        <div>
+          <h4 id="titleDrill">{name_drill}</h4>
+        </div>
+        <div className="imgdrill">
+          <img src={image} alt="delete" role="presentation" id="drillImg" />
+        </div>
       </div>
-      <div className="imgdrill">
-        <img src={image} alt="delete" role="presentation" id="drillImg" />
-      </div>
-    </div>
+    </motion.div>
   );
 }
 
